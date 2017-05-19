3a67eb5abc8b463aba31d5100c17295f477c7746
Dustin Sallings <dustin@spy.net>
2017-05-18 20:52:56 -0700

    Block if an interval is specified

--- uploader/uploader.go
+++ uploader/uploader.go
@@ -449,4 +449,8 @@ func main() {
 	if err := repeatedly(ctx, sto, "upload clips", uploadClips); err != nil {
 		log.Printf("Could not do initial cilp upload: %v", err)
 	}
+
+	if *interval > 0 {
+		select {}
+	}
 }
